4145cca009645e1a496bb548138859693b2a1435
Joseph Mingrone <jrm@ftfl.ca>
2015-03-11 22:22:27 -0300

    Remove shell commands from variables and remove backtick

--- ml_mail.sh
+++ ml_mail.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-## customize these variables
-host=gly ## use . for local host
+## customize these
+## use host=. for local host
+host=my_host
 path=~/mail/new
-port=44422
-ssh=/usr/bin/ssh
-user=jrm
-
+port=22
+user=me
 interval=30
-if [ ${host} = '.' ]; then
-    nmc="ls ${path} | wc -l"
-else
-    nmc="${ssh} -p ${port} -x -o ConnectTimeout=1 ${user}@gly 'ls ${path} | wc -l'"
-fi
-stump_pid=`pgrep -a -n stumpwm`
 
-# while stumpwm is still running
+stump_pid=$(pgrep -a -n stumpwm)
+
+## while stumpwm is still running
 while kill -0 $stump_pid > /dev/null 2>&1; do
-    nm=`eval ${nmc}`
-    echo $nm
+    if [ ${host} = '.' ]; then
+	echo $(ls ${path} | wc -l)
+    else
+	echo $(/usr/bin/ssh -p ${port} -x -o ConnectTimeout=1 ${user}@${host} \
+			    "ls ${path} | wc -l")
+    fi
     sleep ${interval}
 done
