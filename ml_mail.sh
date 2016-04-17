1e86e9672950b3da6371060f303dc8cbc2509b47
Joseph Mingrone <jrm@ftfl.ca>
2016-04-17 02:57:38 -0300

    clean up

--- ml_mail.sh
+++ ml_mail.sh
@@ -12,18 +12,18 @@ port=22
 user=me
 interval=10
 
-stump_pid=$(pgrep -a -n stumpwm)
+stump_pid="$(pgrep -a -n stumpwm)"
 
 ## while stumpwm is still running
-while kill -0 $stump_pid > /dev/null 2>&1; do
-    if [ ${host} = '.' ]; then
-	newcount=$(ls ${path} | wc -l)
+while kill -0 "$stump_pid" > /dev/null 2>&1; do
+    if [ "$host" = '.' ]; then
+	newcount="$(ls ${path} | wc -l)"
     else
-	newcount=$(/usr/bin/ssh -p ${port} -x -o ConnectTimeout=1
-		   ${user}@${host} "ls ${path} | wc -l")
+	newcount=$(/usr/bin/ssh -p "$port" -x -o ConnectTimeout=1
+		   "$user"@"$host" "ls $path | wc -l")
     fi
-    [ ${newcount} -gt ${count} ] && ${ap} ${nms} > /dev/null 2>&1
-    count=${newcount}
-    echo $count
-    sleep ${interval}
+    [ "$newcount" -gt "$count" ] && "$ap" "$nms" > /dev/null 2>&1
+    count="$newcount"
+    echo "$count"
+    sleep "$interval"
 done
